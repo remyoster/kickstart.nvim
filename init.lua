3e7f1b924075ea882f4cc8026f5b00d61baa7f35
roster <rmyoster@gmail.com>
2025-08-14 08:57:47 +0200

    chore: add eslint format

--- init.lua
+++ init.lua
@@ -775,7 +775,7 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'eslint', 'prettierd', 'prettier', stop_after_first = false },
       },
     },
   },
